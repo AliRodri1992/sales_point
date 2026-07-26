92c533860fdfc4c33ed862dbaac9e79c00cb23d0
AliRodri1992 <ali.rodri.vasquez@gmail.com>
2026-07-26 14:12:05 -0600

    feat(user): Add user type, status enums and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,33 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :session_limitable,
+         :recoverable, :rememberable, :validatable, :trackable
+
+  enum :user_type,
+       {
+         employee: 'employee',
+         customer: 'customer',
+         supplier: 'supplier'
+       },
+       validate: true
+
+  enum :status,
+       {
+         active: 'active',
+         suspended: 'suspended',
+         blocked: 'blocked',
+         deleted: 'deleted'
+       },
+       validate: true
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates :user_type,
+            presence: true
+
+  validates :status,
+            presence: true
 end
