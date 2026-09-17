5efb0ddb2f3e44269ed839aa44a4e4e8580e3ded
AliRodri1992 <ali.rodri.vasquez@gmail.com>
2026-09-17 11:00:29 -0600

    feat: Integrate landing page data into home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @features = ::LandingPage.features
+    @modules = ::LandingPage.modules
+    @testimonials = ::LandingPage.testimonials
+    @plans = ::LandingPage.plans
+    @questions = ::LandingPage.questions
   end
 end
