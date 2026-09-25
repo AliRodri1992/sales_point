2998c1e26774b1dfd5321c3855faab7ec84bcca1
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2026-09-25 01:51:39 -0600

    feat(branches): improve branch notifications and addresses

--- app/controllers/admin/branches_controller.rb
+++ app/controllers/admin/branches_controller.rb
@@ -7,33 +7,40 @@ module Admin
     before_action :set_branch, only: %i[update destroy]
 
     def index
-      @branches = Branch.where(deleted_at: nil).order(:name)
+      @branches = Branch.where(deleted_at: nil).includes(:address).order(:name)
       @branch = Branch.new
+      @branch.build_address
     end
 
     def create
       @branch = Branch.new(branch_params)
 
       if @branch.save
-        redirect_to admin_branches_path, notice: 'Sucursal creada correctamente.'
+        notify_branch_change('created')
+        redirect_to admin_branches_path,
+                    flash: { swal_message: 'Sucursal creada correctamente.' }
       else
-        @branches = Branch.where(deleted_at: nil).order(:name)
+        prepare_index
         render :index, status: :unprocessable_content
       end
     end
 
     def update
       if @branch.update(branch_params)
-        redirect_to admin_branches_path, notice: 'Sucursal actualizada correctamente.'
+        notify_branch_change('updated')
+        redirect_to admin_branches_path,
+                    flash: { swal_message: 'Sucursal actualizada correctamente.' }
       else
-        @branches = Branch.where(deleted_at: nil).order(:name)
+        prepare_index
         render :index, status: :unprocessable_content
       end
     end
 
     def destroy
       @branch.update!(deleted_at: Time.current)
-      redirect_to admin_branches_path, notice: 'Sucursal eliminada correctamente.'
+      notify_branch_change('deleted')
+      redirect_to admin_branches_path,
+                  flash: { swal_message: 'Sucursal eliminada correctamente.' }
     end
 
     private
@@ -42,8 +49,34 @@ module Admin
       @branch = Branch.where(deleted_at: nil).find(params[:id])
     end
 
+    def prepare_index
+      @branches = Branch.where(deleted_at: nil).includes(:address).order(:name)
+      @branch.build_address unless @branch.address
+    end
+
     def branch_params
-      params.expect(branch: %i[name phone address status])
+      params.expect(
+        branch: [
+          :name,
+          :phone,
+          :status,
+          address_attributes: %i[
+            id
+            street
+            exterior_number
+            interior_number
+            neighborhood
+            city
+            state
+            country
+            postal_code
+          ]
+        ]
+      )
+    end
+
+    def notify_branch_change(action)
+      BranchNotification.with(branch: @branch, action: action).deliver(current_user)
     end
   end
 end
