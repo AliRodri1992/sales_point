b45eba7a2b327ec766c9b56ad2cff3547fb6baa1
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2026-09-25 01:51:21 -0600

    feat(branches): improve branch notifications and addresses

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,9 @@
 class Branch < ApplicationRecord
+  has_one :address, as: :addressable, dependent: :destroy
+  accepts_nested_attributes_for :address
+
   has_many :user_roles, dependent: :restrict_with_exception
   has_many :users, through: :user_roles
+
+  validates :name, presence: true
 end
